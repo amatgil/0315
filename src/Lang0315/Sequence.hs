9bbffbe506ebc349608ca9de198645cc23be52a3
RubenVerg <RubenVerg@outlook.com>
2026-01-15 09:53:06 +0100

    More easy sequences

--- src/Lang0315/Sequence.hs
+++ src/Lang0315/Sequence.hs
@@ -106,17 +106,36 @@ module Lang0315.Sequence
 , a001097
 , a006882
 , a011557
+, a000262
+, a005811
+, a144944
+, a001003
+, a000196
+, a001481
+, a005100
+, a001037
+, a000594
+, a000688
+, a000001
+, a000031
+, a000058
+, a008279
+, a001057
+, a000161
+, a001489
+, a001478
 ) where
 
 import Control.Monad ((>=>))
 import Data.Bifunctor (bimap)
-import Data.List (genericIndex, genericReplicate, genericLength, sort, sortOn)
+import Data.List (genericIndex, genericReplicate, genericLength, sort, sortOn, group)
 import Data.Ord (Down(..))
 import Data.Maybe (isNothing, isJust, catMaybes)
 import Data.Ratio
 import Data.Universe.Helpers (diagonals)
 
 import Data.Bits (popCount)
+import Data.Bits.Bitwise (toListLE)
 import Math.NumberTheory.Primes (nextPrime, primes, unPrime, factorise, isPrime, UniqueFactorisation)
 import Math.NumberTheory.Primes.Counting (primeCount, nthPrime)
 import qualified Math.NumberTheory.ArithmeticFunctions as AF
@@ -191,6 +210,12 @@ binomial n k = Rec.binomialLine n `genericIndex` k
 stirling2 :: Integer -> Integer -> Integer
 stirling2 n k = Rec.stirling2 `infiniteIndex` n `genericIndex` k
 
+superCatalanT :: [[Integer]]
+superCatalanT = iterate f [1] where f us = vs ++ [last vs] where vs = scanl1 (+) $ zipWith (+) us $ 0 : us
+
+superCatalan :: Integer -> Integer -> Integer
+superCatalan n k = superCatalanT `genericIndex` n `genericIndex` k
+
 infiniteIndex :: IL.Infinite a -> Integer -> a
 infiniteIndex xs i | i < 0 = error "Negative index!"
                    | otherwise = IL.foldr (\x acc m -> if m == 0 then x else acc (m - 1)) xs i
@@ -211,6 +236,16 @@ dedekindPsiHelperA = AF.multiplicative (\p _ -> 1 + 1 % unPrime p)
 
 dedekindPsi :: (UniqueFactorisation n, Integral n) => n -> n
 dedekindPsi n = numerator $ n % 1 * AF.runFunction dedekindPsiHelperA n
+
+intSquareRoots :: [Integer]
+intSquareRoots = concat $ zipWith genericReplicate [1 :: Integer, 3..] [0..]
+
+intSquareRoot :: Integer -> Integer
+intSquareRoot n | n < 0 = 0
+                | otherwise = intSquareRoots `genericIndex` n
+
+isSquare :: Integer -> Bool
+isSquare n = let r = intSquareRoot n in r * r == n
 
 maximumAmountOfDigits :: Int
 maximumAmountOfDigits = 1024
@@ -243,7 +278,9 @@ a000312, a000302, a000670, a001006, a010060, a001065, a055642, a000079, a100995,
 a023443, a000326, a000166, a000330, a002620, a001511, a004526, a000085, a001227, a001906 :: Sequence
 a000124, a001405, a000583, a018252, a001157, a001700, a008292, a005101, a001615, a003418 :: Sequence
 a000169, a246655, a027641, a027642, a000272, a000004, a000204, a000069, a002322, a001969 :: Sequence
-a000002, a003056, a000593, a001097, a006882, a011557 :: Sequence
+a000002, a003056, a000593, a001097, a006882, a011557, a000262, a005811, a144944, a001003 :: Sequence
+a000196, a001481, a005100, a001037, a000594, a000688, a000001, a000031, a000058, a008279 :: Sequence
+a001057, a000161, a001489, a001478 :: Sequence
 a000012 = Sequence $ repeat 1
 a001477 = Sequence $ enumFrom 0
 a000027 = Sequence $ enumFrom 1
@@ -260,7 +297,7 @@ a001222 = Sequence $ ofPositive $ fromIntegral . AF.bigOmega
 a000142 = Sequence $ ofIndices $ infiniteIndex Rec.factorial
 a001221 = Sequence $ ofPositive AF.smallOmega
 a000720 = Sequence $ ofPositive primeCount
-a007318 = Sequence $ ofIndices $ genericIndex $ concat $ IL.toList Rec.binomial
+a007318 = Sequence $ concat $ IL.toList Rec.binomial
 a000120 = Sequence $ ofIndices $ fromIntegral . popCount
 a005117 = Sequence $ ofIndices $ genericIndex $ AF.nFrees 2
 a002110 = Sequence $ ofIndices $ \case
@@ -303,7 +340,7 @@ a000396 = Sequence $ known ++ filter (\n -> AF.sigma 1 n == 2 * n) (enumFrom $ 1
 a000043 = Sequence $ filter (\n -> isJust $ isPrime $ (2 :: Integer) ^ n - 1) $ enumFrom 1
 a001764 = Sequence $ ofIndices $ \n -> binomial (3 * n) n `div` (2 * n + 1)
 a001147 = Sequence $ scanl (\acc n -> acc * (2 * n + 1)) 1 $ enumFrom 0
-a008277 = Sequence $ ofIndices $ genericIndex $ concatMap (drop 1) (IL.toList Rec.stirling2)
+a008277 = Sequence $ concatMap (drop 1) (IL.toList Rec.stirling2)
 a000312 = Sequence $ ofIndices $ \n -> n ^ n
 a000302 = Sequence $ scanl (*) 1 $ repeat 4
 a000670 = Sequence $ ofIndices $ \n -> sum $ map (\k -> stirling2 n k * Rec.factorial `infiniteIndex` k) [0..n]
@@ -334,7 +371,7 @@ a000583 = Sequence $ ofIndices $ \n -> n * n * n * n
 a018252 = Sequence $ filter (isNothing . isPrime) $ enumFrom 1
 a001157 = Sequence $ ofPositive $ AF.sigma 2
 a001700 = Sequence $ ofIndices $ \n -> binomial (2 * n + 1) (n + 1)
-a008292 = Sequence $ ofIndices $ genericIndex $ concat $ IL.toList Rec.eulerian1
+a008292 = Sequence $ concat $ IL.toList Rec.eulerian1
 a005101 = Sequence $ filter (\n -> AF.sigma 1 n > 2 * n) $ enumFrom 1
 a001615 = Sequence $ ofPositive dedekindPsi
 a003418 = Sequence $ ofIndices $ \n -> foldl lcm 1 [1..n]
@@ -354,3 +391,21 @@ a000593 = Sequence $ ofPositive $ \n -> AF.sigma 1 n - if even n then 2 * AF.sig
 a001097 = Sequence $ map unPrime $ filter (\p -> let p' = unPrime p in unPrime (pred p) == p' - 2 || unPrime (succ p) == p' + 2) $ drop 1 primes
 a006882 = Sequence df where df = 1 : 1 : zipWith (*) [2..] df
 a011557 = Sequence $ scanl (*) 1 $ repeat 10
+a000262 = Sequence sol where sol = 1 : 1 : zipWith subtract (zipWith (*) [1..] $ zipWith (*) [0..] sol) (zipWith (*) [3, 5..] $ drop 1 sol)
+a005811 = Sequence $ 0 : ofPositive (genericLength . group . toListLE)
+a144944 = Sequence $ concat superCatalanT
+a001003 = Sequence $ ofIndices $ \n -> superCatalan n n
+a000196 = Sequence intSquareRoots
+a001481 = Sequence $ filter (\n -> any isSquare [n - k * k | k <- [0..intSquareRoot n]]) $ enumFrom 0
+a005100 = Sequence $ filter (\n -> AF.sigma 1 n < 2 * n) $ enumFrom 1
+a001037 = Sequence $ 1 : ofPositive (\n -> (`div` n) $ sum $ map (\d -> AF.runMoebius (AF.moebius $ n `div` d) * 2 ^ d) $ AF.divisorsList n)
+a000594 = Sequence $ ofPositive AF.ramanujan
+a000688 = Sequence $ ofPositive $ AF.runFunction $ AF.multiplicative (\_ k -> Rec.partition `infiniteIndex` fromIntegral k)
+a000001 = Sequence [0, 1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51, 1, 2, 1, 14, 1, 2, 2, 14, 1, 6, 1, 4, 2, 2, 1, 52, 2, 5, 1, 5, 1, 15, 2, 13, 2, 2, 1, 13, 1, 2, 4, 267, 1, 4, 1, 5, 1, 4, 1, 50, 1, 2, 3, 4, 1, 6, 1, 52, 15, 2, 1, 15, 1, 2, 1, 12, 1, 10, 1, 4, 2, 2, 1, 231, 1, 5, 2, 16, 1, 4, 1, 14, 2, 2, 1, 45, 1, 6, 2, 43, 1, 6, 1, 5, 4, 2, 1, 47, 2, 2, 1, 4, 5, 16, 1, 2328, 2, 4, 1, 10, 1, 2, 5, 15, 1, 4, 1, 11, 1, 2, 1, 197, 1, 2, 6, 5, 1, 13, 1, 12, 2, 4, 2, 18, 1, 2, 1, 238, 1, 55, 1, 5, 2, 2, 1, 57, 2, 4, 5, 4, 1, 4, 2, 42, 1, 2, 1, 37, 1, 4, 2, 12, 1, 6, 1, 4, 13, 4, 1, 1543, 1, 2, 2, 12, 1, 10, 1, 52, 2, 2, 2, 12, 2, 2, 2, 51, 1, 12, 1, 5, 1, 2, 1, 177, 1, 2, 2, 15, 1, 6, 1, 197, 6, 2, 1, 15, 1, 4, 2, 14, 1, 16, 1, 4, 2, 4, 1, 208, 1, 5, 67, 5, 2, 4, 1, 12, 1, 15, 1, 46, 2, 2, 1, 56092, 1, 6, 1, 15, 2, 2, 1, 39, 1, 4, 1, 4, 1, 30, 1, 54, 5, 2, 4, 10, 1, 2, 4, 40, 1, 4, 1, 4, 2, 4, 1, 1045, 2, 4, 2, 5, 1, 23, 1, 14, 5, 2, 1, 49, 2, 2, 1, 42, 2, 10, 1, 9, 2, 6, 1, 61, 1, 2, 4, 4, 1, 4, 1, 1640, 1, 4, 1, 176, 2, 2, 2, 15, 1, 12, 1, 4, 5, 2, 1, 228, 1, 5, 1, 15, 1, 18, 5, 12, 1, 2, 1, 12, 1, 10, 14, 195, 1, 4, 2, 5, 2, 2, 1, 162, 2, 2, 3, 11, 1, 6, 1, 42, 2, 4, 1, 15, 1, 4, 7, 12, 1, 60, 1, 11, 2, 2, 1, 20169, 2, 2, 4, 5, 1, 12, 1, 44, 1, 2, 1, 30, 1, 2, 5, 221, 1, 6, 1, 5, 16, 6, 1, 46, 1, 6, 1, 4, 1, 10, 1, 235, 2, 4, 1, 41, 1, 2, 2, 14, 2, 4, 1, 4, 2, 4, 1, 775, 1, 4, 1, 5, 1, 6, 1, 51, 13, 4, 1, 18, 1, 2, 1, 1396, 1, 34, 1, 5, 2, 2, 1, 54, 1, 2, 5, 11, 1, 12, 1, 51, 4, 2, 1, 55, 1, 4, 2, 12, 1, 6, 2, 11, 2, 2, 1, 1213, 1, 2, 2, 12, 1, 261, 1, 14, 2, 10, 1, 12, 1, 4, 4, 42, 2, 4, 1, 56, 1, 2, 1, 202, 2, 6, 6, 4, 1, 8, 1, 10494213, 15, 2, 1, 15, 1, 4, 1, 49, 1, 10, 1, 4, 6, 2, 1, 170, 2, 4, 2, 9, 1, 4, 1, 12, 1, 2, 2, 119, 1, 2, 2, 246, 1, 24, 1, 5, 4, 16, 1, 39, 1, 2, 2, 4, 1, 16, 1, 180, 1, 2, 1, 10, 1, 2, 49, 12, 1, 12, 1, 11, 1, 4, 2, 8681, 1, 5, 2, 15, 1, 6, 1, 15, 4, 2, 1, 66, 1, 4, 1, 51, 1, 30, 1, 5, 2, 4, 1, 205, 1, 6, 4, 4, 7, 4, 1, 195, 3, 6, 1, 36, 1, 2, 2, 35, 1, 6, 1, 15, 5, 2, 1, 260, 15, 2, 2, 5, 1, 32, 1, 12, 2, 2, 1, 12, 2, 4, 2, 21541, 1, 4, 1, 9, 2, 4, 1, 757, 1, 10, 5, 4, 1, 6, 2, 53, 5, 4, 1, 40, 1, 2, 2, 12, 1, 18, 1, 4, 2, 4, 1, 1280, 1, 2, 17, 16, 1, 4, 1, 53, 1, 4, 1, 51, 1, 15, 2, 42, 2, 8, 1, 5, 4, 2, 1, 44, 1, 2, 1, 36, 1, 62, 1, 1387, 1, 2, 1, 10, 1, 6, 4, 15, 1, 12, 2, 4, 1, 2, 1, 840, 1, 5, 2, 5, 2, 13, 1, 40, 504, 4, 1, 18, 1, 2, 6, 195, 2, 10, 1, 15, 5, 4, 1, 54, 1, 2, 2, 11, 1, 39, 1, 42, 1, 4, 2, 189, 1, 2, 2, 39, 1, 6, 1, 4, 2, 2, 1, 1090235, 1, 12, 1, 5, 1, 16, 4, 15, 5, 2, 1, 53, 1, 4, 5, 172, 1, 4, 1, 5, 1, 4, 2, 137, 1, 2, 1, 4, 1, 24, 1, 1211, 2, 2, 1, 15, 1, 4, 1, 14, 1, 113, 1, 16, 2, 4, 1, 205, 1, 2, 11, 20, 1, 4, 1, 12, 5, 4, 1, 30, 1, 4, 2, 1630, 2, 6, 1, 9, 13, 2, 1, 186, 2, 2, 1, 4, 2, 10, 2, 51, 2, 10, 1, 10, 1, 4, 5, 12, 1, 12, 1, 11, 2, 2, 1, 4725, 1, 2, 3, 9, 1, 8, 1, 14, 4, 4, 5, 18, 1, 2, 1, 221, 1, 68, 1, 15, 1, 2, 1, 61, 2, 4, 15, 4, 1, 4, 1, 19349, 2, 2, 1, 150, 1, 4, 7, 15, 2, 6, 1, 4, 2, 8, 1, 222, 1, 2, 4, 5, 1, 30, 1, 39, 2, 2, 1, 34, 2, 2, 4, 235, 1, 18, 2, 5, 1, 2, 2, 222, 1, 4, 2, 11, 1, 6, 1, 42, 13, 4, 1, 15, 1, 10, 1, 42, 1, 10, 2, 4, 1, 2, 1, 11394, 2, 4, 2, 5, 1, 12, 1, 42, 2, 4, 1, 900, 1, 2, 6, 51, 1, 6, 2, 34, 5, 2, 1, 46, 1, 4, 2, 11, 1, 30, 1, 196, 2, 6, 1, 10, 1, 2, 15, 199, 1, 4, 1, 4, 2, 2, 1, 954, 1, 6, 2, 13, 1, 23, 2, 12, 2, 2, 1, 37, 1, 4, 2, 49487367289, 4, 66, 2, 5, 19, 4, 1, 54, 1, 4, 2, 11, 1, 4, 1, 231, 1, 2, 1, 36, 2, 2, 2, 12, 1, 40, 1, 4, 51, 4, 2, 1028, 1, 5, 1, 15, 1, 10, 1, 35, 2, 4, 1, 12, 1, 4, 4, 42, 1, 4, 2, 5, 1, 10, 1, 583, 2, 2, 6, 4, 2, 6, 1, 1681, 6, 4, 1, 77, 1, 2, 2, 15, 1, 16, 1, 51, 2, 4, 1, 170, 1, 4, 5, 5, 1, 12, 1, 12, 2, 2, 1, 46, 1, 4, 2, 1092, 1, 8, 1, 5, 14, 2, 2, 39, 1, 4, 2, 4, 1, 254, 1, 42, 2, 2, 1, 41, 1, 2, 5, 39, 1, 4, 1, 11, 1, 10, 1, 157877, 1, 2, 4, 16, 1, 6, 1, 49, 13, 4, 1, 18, 1, 4, 1, 53, 1, 32, 1, 5, 1, 2, 2, 279, 1, 4, 2, 11, 1, 4, 3, 235, 2, 2, 1, 99, 1, 8, 2, 14, 1, 6, 1, 11, 14, 2, 1, 1040, 1, 2, 1, 13, 2, 16, 1, 12, 5, 27, 1, 12, 1, 2, 69, 1387, 1, 16, 1, 20, 2, 4, 1, 164, 4, 2, 2, 4, 1, 12, 1, 153, 2, 2, 1, 15, 1, 2, 2, 51, 1, 30, 1, 4, 1, 4, 1, 1460, 1, 55, 4, 5, 1, 12, 2, 14, 1, 4, 1, 131, 1, 2, 2, 42, 3, 6, 1, 5, 5, 4, 1, 44, 1, 10, 3, 11, 1, 10, 1, 1116461, 5, 2, 1, 10, 1, 2, 4, 35, 1, 12, 1, 11, 1, 2, 1, 3609, 1, 4, 2, 50, 1, 24, 1, 12, 2, 2, 1, 18, 1, 6, 2, 244, 1, 18, 1, 9, 2, 2, 1, 181, 1, 2, 51, 4, 2, 12, 1, 42, 1, 8, 5, 61, 1, 4, 1, 12, 1, 6, 1, 11, 2, 4, 1, 11720, 1, 2, 1, 5, 1, 112, 1, 52, 1, 2, 2, 12, 1, 4, 4, 245, 1, 4, 1, 9, 5, 2, 1, 211, 2, 4, 2, 38, 1, 6, 15, 195, 15, 6, 2, 29, 1, 2, 1, 14, 1, 32, 1, 4, 2, 4, 1, 198, 1, 4, 8, 5, 1, 4, 1, 153, 1, 2, 1, 227, 2, 4, 5, 19324, 1, 8, 1, 5, 4, 4, 1, 39, 1, 2, 2, 15, 4, 16, 1, 53, 6, 4, 1, 40, 1, 12, 5, 12, 1, 4, 2, 4, 1, 2, 1, 5958, 1, 4, 5, 12, 2, 6, 1, 14, 4, 10, 1, 40, 1, 2, 2, 179, 1, 1798, 1, 15, 2, 4, 1, 61, 1, 2, 5, 4, 1, 46, 1, 1387, 1, 6, 2, 36, 2, 2, 1, 49, 1, 24, 1, 11, 10, 2, 1, 222, 1, 4, 3, 5, 1, 10, 1, 41, 2, 4, 1, 174, 1, 2, 2, 195, 2, 4, 1, 15, 1, 6, 1, 889, 1, 2, 2, 4, 1, 12, 2, 178, 13, 2, 1, 15, 4, 4, 1, 12, 1, 20, 1, 4, 5, 4, 1, 408641062, 1, 2, 60, 36, 1, 4, 1, 15, 2, 2, 1, 46, 1, 16, 1, 54, 1, 24, 2, 5, 2, 4, 1, 221, 1, 4, 1, 11, 1, 30, 1, 928, 2, 4, 1, 10, 2, 2, 13, 14, 1, 4, 1, 11, 2, 6, 1, 697, 1, 4, 3, 5, 1, 8, 1, 12, 5, 2, 2, 64, 1, 4, 2, 10281, 1, 10, 1, 5, 1, 4, 1, 54, 1, 8, 2, 11, 1, 4, 1, 51, 6, 2, 1, 477, 1, 2, 2, 56, 5, 6, 1, 11, 5, 4, 1, 1213, 1, 4, 2, 5, 1, 72, 1, 68, 2, 2, 1, 12, 1, 2, 13, 42, 1, 38, 1, 9, 2, 2, 2, 137, 1, 2, 5, 11, 1, 6, 1, 21507, 5, 10, 1, 15, 1, 4, 1, 34, 2, 60, 2, 4, 5, 2, 1, 1005, 2, 5, 2, 5, 1, 4, 1, 12, 1, 10, 1, 30, 1, 10, 1, 235, 1, 6, 1, 50, 309, 4, 2, 39, 7, 2, 1, 11, 1, 36, 2, 42, 2, 2, 5, 40, 1, 2, 2, 39, 1, 12, 1, 4, 3, 2, 1, 47937, 1, 4, 2, 5, 1, 13, 1, 35, 4, 4, 1, 37, 1, 4, 2, 51, 1, 16, 1, 9, 1, 30, 2, 64, 1, 2, 14, 4, 1, 4, 1, 1285, 1, 2, 1, 228, 1, 2, 5, 53, 1, 8, 2, 4, 2, 2, 4, 260, 1, 6, 1, 15, 1, 110, 1, 12, 2, 4, 1, 12, 1, 4, 5, 1083553, 1, 12, 1, 5, 1, 4, 1, 749, 1, 4, 2, 11, 3, 30, 1, 54, 13, 6, 1, 15, 2, 2, 9, 12, 1, 10, 1, 35, 2, 2, 1, 1264, 2, 4, 6, 5, 1, 18, 1, 14, 2, 4, 1, 117, 1, 2, 2, 178, 1, 6, 1, 5, 4, 4, 1, 162, 2, 10, 1, 4, 1, 16, 1, 1630, 2, 2, 2, 56, 1, 10, 15, 15, 1, 4, 1, 4, 2, 12, 1, 1096, 1, 2, 21, 9, 1, 6, 1, 39, 5, 2, 1, 18, 1, 4, 2, 195, 1, 120, 1, 9, 2, 2, 1, 54, 1, 4, 4, 36, 1, 4, 1, 186, 2, 2, 1, 36, 1, 6, 15, 12, 1, 8, 1, 4, 5, 4, 1, 241004, 1, 5, 1, 15, 4, 10, 1, 15, 2, 4, 1, 34, 1, 2, 4, 167, 1, 12, 1, 15, 1, 2, 1, 3973, 1, 4, 1, 4, 1, 40, 1, 235, 11, 2, 1, 15, 1, 6, 1, 144, 1, 18, 1, 4, 2, 2, 2, 203, 1, 4, 15, 15, 1, 12, 2, 39, 1, 4, 1, 120, 1, 2, 2, 1388, 1, 6, 1, 13, 4, 4, 1, 39, 1, 2, 5, 4, 1, 66, 1, 963, 1, 8, 1, 10, 2, 4, 4, 12, 2, 12, 1, 4, 2, 4, 2, 6538, 1, 2, 2, 20, 1, 6, 2, 46, 63, 2, 1, 88, 1, 12, 1, 42, 1, 10, 2, 5, 5, 2, 1, 175, 2, 2, 2, 11, 1, 12, 1, undefined]
+a000031 = Sequence $ 1 : ofPositive (\n -> (`div` n) $ sum $ map (\d -> AF.totient n * 2 ^ (n `div` d)) $ AF.divisorsList n)
+a000058 = Sequence $ iterate (\n -> n * n - n + 1) 2
+a008279 = Sequence $ concatMap (zipWith (*) $ IL.toList Rec.factorial) (IL.toList Rec.binomial)
+a001057 = Sequence $ 0 : concatMap (\x -> [x, -x]) (enumFrom 1)
+a000161 = Sequence $ ofIndices $ \n -> genericLength $ filter isSquare [n - k * k | k <- [0..intSquareRoot n]]
+a001489 = Sequence $ ofIndices negate
+a001478 = Sequence $ ofPositive negate
